f7d0554b916f7911bf33b2054c0c2c3b77ecc5c4
GuiilhermeOG <guilhermeguimarae342s@gmail.com>
2026-02-19 16:46:50 -0300

    initial exercises

---
+++ challenge2.js
@@ -0,0 +1,10 @@
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const isEven = (number) => {
+  return number % 2 === 0;
+};
+
+console.log(multiply(4, 5));
+console.log(isEven(10));
---
+++ warmup.js
@@ -0,0 +1,10 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const isAdult = (age) => {
+  return age >= 18;
+};
+
+console.log(sum(2, 3));
+console.log(isAdult(20));
